e8f1bf73ff9a2ee01b8febe5ea09d3168342459e
Niclas Hedam <nhed@itu.dk>
2020-12-16 10:01:53 +0100

    Added a glibc function

--- src/loader.c
+++ src/loader.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+  // Load our newly compiled eBPF program
   if (load_bpf_file("src/bpf_program.o") != 0) {
     printf("The kernel didn't load the BPF program\n");
     return -1;
@@ -11,12 +12,13 @@ int main(int argc, char **argv) {
   printf("eBPF will listen to force kills for the next 30 seconds!\n");
   sleep(30);
 
-  int fd = map_fd[0];
-  unsigned int key = -1, prev_key, val;
+  // map_fd is a global variable containing all eBPF map file descriptors
+  int fd = map_fd[0], val;
+  long key = -1, prev_key;
 
+  // Iterate over all keys in the map
   while(bpf_map_get_next_key(fd, &prev_key, &key) == 0) {
-    bpf_map_lookup_elem(fd, &key, &val);
-    printf("%u was forcefully killed!\n", key);
+    printf("%ld was forcefully killed!\n", key);
     prev_key = key;
   }
 
