f9112f022a1ab6b35fe753a73bcd79ee13033863
Niclas Hedam <nhed@itu.dk>
2021-01-29 10:53:27 +0100

    GPL and glibc comment to clarify

--- src/bpf_program.c
+++ src/bpf_program.c
@@ -25,15 +25,21 @@ int bpf_prog(struct syscalls_enter_kill_args *ctx) {
   // Ignore normal program terminations
   if(ctx->sig != 9) return 0;
 
-  // We can call glibc functions in eBPF
+  // We can call glibc functions in eBPF program if and only if
+  // they are not too large and do not use any of the risky operations
+  // disallowed by the eBPF verifier. These include, among others,
+  // complex loops and floats.
   long key = labs(ctx->pid);
   int val = 1;
 
-  // Mark the PID as killed in the map
+  // Mark the PID as killed in the map.
+  // This will create an entry where the killed PID is set to 1.
   bpf_map_update_elem(&kill_map, &key, &val, BPF_NOEXIST);
 
   return 0;
 }
 
-// All eBPF programs must be GPL licensed
+// Some eBPF programs must be GPL licensed. This depends on program types,
+// eBPF helpers used and among other things. As this eBPF program is
+// integrating with tracepoints, it must be GPL.
 char _license[] SEC("license") = "GPL";
